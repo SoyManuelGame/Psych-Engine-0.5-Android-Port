2dd32b21ea5e290430dbe331034c400a84dfa5fc
SoyManuelGame <95936700+SoyManuelGame@users.noreply.github.com>
2021-12-27 09:47:24 -0500

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -11,6 +11,7 @@ import openfl.events.Event;
 import sys.FileSystem;
 import lime.app.Application;
 import lime.system.System;
+import android.*;
 
 class Main extends Sprite
 {
@@ -25,30 +26,22 @@ class Main extends Sprite
 	public static var fpsVar:FPS;
 	private static var dataPath:String = null;
 
-    static public function getDataPath():String 
-    {
-        if (dataPath != null && dataPath.length > 0) 
+        static public function getDataPath():String 
         {
-            return dataPath;
-        } 
-        else 
-        {
-        	#if mobile
-            if (FileSystem.exists("/storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/")) 
+            if (dataPath != null && dataPath.length > 0) 
             {
-                dataPath = "/storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/";
+                return dataPath;
             } 
             else 
             {
-                Application.current.window.alert("couldn't find directory: " + "/storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/" + "\n" + "try creating it and copying assets/assets, assets/mods from apk to it","an ERROR occured");
-                dataPath = System.applicationStorageDirectory;
+                 #if android
+                 dataPath = "/storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/";
+                 #elseif desktop
+                 dataPath = "";
+                 #end
             }
-            #else
-                dataPath = "";
-            #end
+            return dataPath;
         }
-        return dataPath;
-    }
 
 	public static function main():Void
 	{
@@ -96,6 +89,37 @@ class Main extends Sprite
 		#if !debug
 		initialState = TitleState;
 		#end
+
+                #if android
+                AndroidTools.requestPermission(Permissions.READ_EXTERNAL_STORAGE);
+
+                //For Stupid Kids
+                if (!FileSystem.exists("/storage/emulated/0/Android/data/" + Application.current.meta.get("packageName")))
+                {
+                    Application.current.window.alert("Try creating A folder Called " + Application.current.meta.get("packageName") + " in Android/data/" + "\n" + "Press Ok To Close The App", "Check Directory Error");
+                    System.exit(0);//Will close the game
+                }
+                else if (!FileSystem.exists("/storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/"))
+                {
+                    Application.current.window.alert("Try creating A folder Called Files in Android/data/" + Application.current.meta.get("packageName") + "\n" + "Press Ok To Close The App", "Check Directory Error");
+                    System.exit(0);//Will close the game
+                }
+                else if (!FileSystem.exists(Main.getDataPath() + "assets"))
+                {
+                    Application.current.window.alert("Try copying assets/assets from apk to " + " /storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/" + "\n" + "Press Ok To Close The App", "Check Directory Error");
+                    System.exit(0);//Will close the game
+                }
+                else if (!FileSystem.exists(Main.getDataPath() + "mods"))
+                {
+                    Application.current.window.alert("Try copying assets/mods from apk to " + " /storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/" + "\n" + "Press Ok To Close The App", "Check Directory Error");
+                    System.exit(0);//Will close the game
+                }
+                else
+                {
+                    if (!FileSystem.exists(Main.getDataPath() + "yourthings"))
+	            FileSystem.createDirectory(Main.getDataPath() + "yourthings");                   
+                }
+                #end
 
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
